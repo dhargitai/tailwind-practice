3b91ce5f7ee94c3bf50ffd527c93f5aef8dc17fc
David Hargitai <david@leadforward.hu>
2021-03-06 08:29:30 +0100

    Blog & post page

--- growthbadger-blog/tailwind.config.js
+++ growthbadger-blog/tailwind.config.js
@@ -1,11 +1,19 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: colors.orange,
+        lightBlue: colors.lightBlue,
+        warmGray: colors.warmGray,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
